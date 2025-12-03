8575c0797f4fd1af57bf2aabd9ee378c8d58b532
Sanjana Gamit <sanjanabengamit123@gmail.com>
2025-12-03 09:52:42 +0530

    Add features to load and search posts from API

--- script.js
+++ script.js
@@ -51,3 +51,51 @@ window.addEventListener("scroll", () => {
         }
     });
 });
+/* FEATURE 1 – Load Multiple Posts (Cards) */
+document.getElementById("loadAllPostsBtn").addEventListener("click", () => {
+    
+    fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(res => res.json())
+        .then(posts => {
+            const container = document.getElementById("postsContainer");
+            container.innerHTML = ""; // clear previous posts
+
+            posts.slice(0, 12).forEach(post => {  // show first 12 cards
+                const card = `
+                    <div class="card">
+                        <h3>${post.title}</h3>
+                        <p>${post.body}</p>
+                        <p><b>User ID:</b> ${post.userId}</p>
+                    </div>
+                `;
+                container.innerHTML += card;
+            });
+        })
+        .catch(err => console.log(err));
+});
+/* FEATURE 2 – Search API by ID */
+document.getElementById("searchPostBtn").addEventListener("click", () => {
+    const id = document.getElementById("postIdInput").value;
+    const output = document.getElementById("searchResult");
+
+    if (id === "" || id <= 0 || id > 100) {
+        output.style.display = "block";
+        output.innerHTML = "<p>Please enter a valid Post ID (1–100)</p>";
+        return;
+    }
+
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        .then(res => res.json())
+        .then(data => {
+            output.style.display = "block";
+            output.innerHTML = `
+                <h3>${data.title}</h3>
+                <p>${data.body}</p>
+                <p><b>User ID:</b> ${data.userId}</p>
+            `;
+        })
+        .catch(err => {
+            output.innerHTML = "<p>Error loading data</p>";
+            console.log(err);
+        });
+});
